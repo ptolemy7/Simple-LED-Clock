eb4f236d132f4f55952b7ceec7848010c48ba389
Dalton Sconce <dsconce@protonmail.com>
2019-06-19 16:16:51 -0400

    The numbers need worked on, but so far so good

--- src/testing.py
+++ src/testing.py
@@ -1,3 +1,7 @@
 import lib
+import time
+for q in range(0,10):
+	lib.print_to_screen(10,lib.to_be_on(q))
+	time.sleep(1)
+	lib.print_to_screen(0,lib.full_screen)
 
-lib.print_to_screen(100,lib.full_screen)
