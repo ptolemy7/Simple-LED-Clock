c81b0fe5ab801d27d9e8a20d2e50a8d5d3315085
Dalton Sconce <dscone@protonmail.com>
2019-07-25 00:57:39 -0400

    Tweaked the install script a bit, I believe this is ready to merege into master

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ##Make sure everything is current
+if [[ ! -a /usr/bin/git ]]; then
+	echo "Error, you must manually install git (sudo apt install git or pacman -S git)"
+	exit "Git not installed"
 /usr/bin/git pull origin master
 ##Make some simlinks
 sudo install $1
@@ -17,7 +20,7 @@ cd ../install_files/
 ##installs all of the dependencies, python3 and python3-pip, and the python dependencies
 install() {
 	if [[ $1 == 'arch' ]]; then
-		pacman -S $(cat dependencies.system-arch)
+		pacman -S $(cat dependencies.system-arch) --noconfirm
 	fi
 	if [[ $1 == 'deb' ]]; then
 		apt update
@@ -26,11 +29,15 @@ install() {
 	fi
 	pip3 install $(cat dependencies.python)
 }
-isntal_from_aur() {
+install_gpio_aur() {
+	echo "This will only install python-rasberry-gpio from the AUR"
 	cd $HOME
 	git clone https://aur.archlinux.org/python-raspberry-gpio.git
 	cd python-raspberry-gpio
 	makepkg -s
+	sudo pacman -U *pkg.tar.xz
 }
-
+install_gpio_aur
 echo "Done!! It *should* work now"
+echo "Note: There may be other steps if one is using Arch Linux for ARM,
+The wiki will be your best friend in this regard"
